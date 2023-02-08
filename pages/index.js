5e933a53c9b341e3eb998e7751587055ed62fda9
EvelinJoemaa <evelin.joemaa@vikk.ee>
2023-02-08 22:28:07 +0200

    Pre-rendering and Data Fetching

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-export default function Home() {
+
+import { getSortedPostsData } from '../lib/posts';
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData,
+        },
+    };
+}
+
+
+export default function Home({ allPostsData }) {
   return (
       <Layout home>
         <Head>
           <title>{siteTitle}</title>
         </Head>
+
         <section className={utilStyles.headingMd}>
           <p>¡Hola! I'm Evelin. I'm an English major making a turn towards software development, with special interest in quality assurance.</p>
           <p>
@@ -14,6 +27,23 @@ export default function Home() {
             <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
           </p>
         </section>
+
+
+          <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+              <h2 className={utilStyles.headingLg}>Blog</h2>
+              <ul className={utilStyles.list}>
+                  {allPostsData.map(({ id, date, title }) => (
+                      <li className={utilStyles.listItem} key={id}>
+                          {title}
+                          <br />
+                          {id}
+                          <br />
+                          {date}
+                      </li>
+                  ))}
+              </ul>
+          </section>
+
       </Layout>
   );
 }
